b9dd0445f4f98beffc291cb9361ea19c45eae104
Marcelo Cantos <marcelo.cantos@gmail.com>
2023-10-27 21:59:41 +1000

    remove testify dependency; go get -u (#4)

--- hashable.go
+++ hashable.go
@@ -1,6 +1,8 @@
 package hash
 
+type Seed = uintptr
+
 // Hashable represents a type that can evaluate its own hash.
 type Hashable interface {
-	Hash(seed uintptr) uintptr
+	Hash(seed Seed) Seed
 }
